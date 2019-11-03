9e291543b44c33bd5c7567585bbaa139aa968b97
Thomas Siller <siller.thomas@gmx.net>
2019-11-03 14:06:21 +0100

    initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { ErrorMapper } from "utils/ErrorMapper";
+//import { ErrorMapper } from "utils/ErrorMapper";
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
-export const loop = ErrorMapper.wrapLoop(() => {
+/*export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current game tick is ${Game.time}`);
 
   // Automatically delete memory of missing creeps
@@ -11,4 +11,8 @@ export const loop = ErrorMapper.wrapLoop(() => {
       delete Memory.creeps[name];
     }
   }
-});
+});*/
+
+export const loop = () => {
+  console.log("test");
+};
--- src/utils/ErrorMapper.ts
+++ src/utils/ErrorMapper.ts
@@ -1,9 +1,10 @@
 // tslint:disable:no-conditional-assignment
 import { SourceMapConsumer } from "source-map";
+import _ from "lodash";
 
 export class ErrorMapper {
   // Cache consumer
-  private static _consumer?: SourceMapConsumer;
+  private static _consumer?: any;
 
   public static get consumer(): SourceMapConsumer {
     if (this._consumer == null) {
